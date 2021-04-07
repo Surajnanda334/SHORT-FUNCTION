768583301a143404d3f01756837123fa720d2d10
Suraj Nanda <50697016+Surajnanda334@users.noreply.github.com>
2021-04-07 20:38:38 +0530

    Tower Of Hanoi Problem

--- towerOfHanoi.cpp
+++ towerOfHanoi.cpp
@@ -2,27 +2,28 @@
 
 using namespace std;
 
-void toh(int s, int h, int d, int &c,int n);
+void toh(int source, int helper, int destination, int &count,int n);        //Function prototype
 
-int main()
+int main()      // Main function
 {
-    int n,s=1,h=2,d=3,c=0;
+    int n,source=1,helper=2,destination=3,count=0;      //Declaring integer values for numbering the towers
+    cout<<"Enter the total number of plates :";
     cin>>n;
-    toh(s,h,d,c,n);
-    cout<<"\nTotal number of steps :"<<c;
+    toh(source,helper,destination,count,n);     //Function call
+    cout<<"\nTotal number of steps :"<<count;       //Displaying number of steps
 }
 
-void toh(int s, int h, int d, int &c, int n)
+void toh(int source, int helper, int destination, int &count, int n)
 {
-    if(n==1)
+    if(n==1)        //When there is only one plate on source tower and all plates are transfered to helper tower
     {
-        cout<<"\nMoving disc "<<n<<" from tower "<<s<<" to "<<d;
-        c++;
+        cout<<"\nMoving disc "<<n<<" from tower "<<source<<" to "<<destination;
+        count++;        //Keeping track of steps
         return;
     }
-    toh(s,d,h,c,n-1);
-    cout<<"\nMoving disc "<<n<<" from tower "<<s<<" to "<<d;
-    c++;
-    toh(h,s,d,c,n-1);
+    toh(source,destination,helper,count,n-1);       //This will transfer n-1 plates from source to helper
+    cout<<"\nMoving disc "<<n<<" from tower "<<source<<" to "<<destination;
+    count++;
+    toh(helper,source,destination,count,n-1);       //Now when n-1 plates are placed on helper tower  we need to move them from helper to destination
     return;
 }
